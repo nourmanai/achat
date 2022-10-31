955afbff3695f5e8f1a8761f8aa752908c361555
anis <anis.hajali@esprit.tn>
2022-10-31 16:39:52 +0100

    test

---
+++ src/test/java/tn/esprit/rh/achat/services/FournisseurServiceImplTest.java
@@ -0,0 +1,126 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.eq;
+
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import tn.esprit.rh.achat.entities.CategorieFournisseur;
+import tn.esprit.rh.achat.entities.DetailFournisseur;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.entities.SecteurActivite;
+import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
+import tn.esprit.rh.achat.repositories.FournisseurRepository;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@ContextConfiguration(classes = {FournisseurServiceImpl.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FournisseurServiceImplTest {
+    @MockBean
+    private DetailFournisseurRepository detailFournisseurRepository;
+
+    @MockBean
+    private FournisseurRepository fournisseurRepository;
+
+    @Autowired
+    private FournisseurServiceImpl fournisseurServiceImpl;
+
+    @MockBean
+    private ProduitRepository produitRepository;
+
+    @MockBean
+    private SecteurActiviteRepository secteurActiviteRepository;
+   
+   @Autowired
+   private IFournisseurService fs;
+
+
+
+
+
+    @Test
+    public void RetriveallFournisseru() throws ParseException {
+        Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setIdFournisseur(1L);
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setCode("123");
+        fournisseur.setFactures(null);
+        fournisseur.setIdFournisseur(1L);
+        fournisseur.setLibelle("lib");
+        fournisseur.setSecteurActivites(null);
+        fournisseur.setCategorieFournisseur(null);
+        
+        Fournisseur fournisseur2 = new Fournisseur();
+        fournisseur2.setIdFournisseur(2L);
+        fournisseur2.setCategorieFournisseur(null);
+        fournisseur2.setCode("123456");
+        fournisseur2.setFactures(null);
+        fournisseur2.setIdFournisseur(1L);
+        fournisseur2.setLibelle("lib");
+        fournisseur2.setSecteurActivites(null);
+        fournisseur.setCategorieFournisseur(null);
+
+
+        
+        ArrayList<Fournisseur> data = new ArrayList<>();
+        data.add(fournisseur);
+        data.add(fournisseur2);
+        when(fournisseurRepository.findAll()).thenReturn(data);
+        List<Fournisseur> resultatService = fournisseurServiceImpl.retrieveAllFournisseurs();
+        assertEquals(2, resultatService.size());
+        assertSame(data, resultatService);
+        verify(fournisseurRepository).findAll();
+    }
+
+    @Test
+    public void deleteFournisseur() {
+    	Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setIdFournisseur(1L);
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setCode("123");
+        fournisseur.setFactures(null);
+        fournisseur.setIdFournisseur(1L);
+        fournisseur.setLibelle("lib");
+        fournisseur.setSecteurActivites(null);
+        fournisseur.setCategorieFournisseur(null);
+        fournisseurServiceImpl.deleteFournisseur(1L);
+        verify(fournisseurRepository, times(1)).deleteById(eq(fournisseur.getIdFournisseur()));
+      
+        
+    }
+    
+   
+    
+}
---
+++ src/test/java/tn/esprit/rh/achat/services/FournisseurServiceJunitTest.java
@@ -0,0 +1,64 @@
+/*package tn.esprit.rh.achat.services;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.entities.SecteurActivite;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FournisseurServiceJunitTest {
+	@Autowired
+	IFournisseurService fs;
+	@Autowired
+	ISecteurActiviteService sc;
+	@Test
+    public void TestAddFournisseur() {
+    	Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setCode("123");
+        fournisseur.setFactures(null);
+        fournisseur.setLibelle("lib");
+        fournisseur.setSecteurActivites(null);
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setDetailFournisseur(null);
+        Fournisseur f = fs.addFournisseur(fournisseur);
+        assertNull(f.getFactures());
+		assertSame("123", f.getCode());      
+		fs.deleteFournisseur(f.getIdFournisseur());
+	}
+	
+	@Test
+    public void retriveall() {
+    	Fournisseur fournisseur = new Fournisseur();
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setCode("123");
+        fournisseur.setFactures(null);
+        fournisseur.setLibelle("lib");
+        fournisseur.setSecteurActivites(null);
+        fournisseur.setCategorieFournisseur(null);
+        fournisseur.setDetailFournisseur(null);
+        Fournisseur fournisseur1 = new Fournisseur();
+        fournisseur1.setCategorieFournisseur(null);
+        fournisseur1.setCode("123");
+        fournisseur1.setFactures(null);
+        fournisseur1.setLibelle("lib");
+        fournisseur1.setSecteurActivites(null);
+        fournisseur1.setCategorieFournisseur(null);
+        fournisseur1.setDetailFournisseur(null);
+        Fournisseur f= fs.addFournisseur(fournisseur);
+        Fournisseur f2 = fs.addFournisseur(fournisseur1);
+        assertEquals(2, fs.retrieveAllFournisseurs().size());
+		fs.deleteFournisseur(f.getIdFournisseur());
+		fs.deleteFournisseur(f2.getIdFournisseur());
+        
+	}
+	
+  
+    
+}
+*/

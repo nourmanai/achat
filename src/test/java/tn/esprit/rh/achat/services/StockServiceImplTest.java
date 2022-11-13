fc96563a07328efc87ae31890db59a5fe92111fd
mahdi <mahhdi.ayadi@esprit.tn>
2022-11-13 14:51:20 +0100

    Test

---
+++ src/test/java/tn/esprit/rh/achat/services/StockServiceImplTest.java
@@ -0,0 +1,88 @@
+package tn.esprit.rh.achat.services;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.repositories.StockRepository;
+import tn.esprit.rh.achat.services.StockServiceImpl;
+
+
+
+@ExtendWith(MockitoExtension.class)
+public class StockServiceImplTest {
+
+
+@Mock
+StockRepository stockRepository;
+
+@InjectMocks
+StockServiceImpl stockService;
+
+
+
+
+
+@Test
+public void retrieveAllStockTest() {
+	when(stockRepository.findAll()).thenReturn(Stream.of(
+            new Stock("libelleStock2", 20, 5),
+            new Stock("libelleStock3", 2055, 555),
+            new Stock("libelleStock3", 2550, 5))
+            .collect(Collectors.toList()));
+	assertEquals(3,stockService.retrieveAllStocks().size());
+	
+}
+
+@Test
+public void addStockTest() {
+	Stock ss = new Stock("libelleStock2", 20, 5);
+	when(stockRepository.save(ss)).thenReturn(ss);
+	assertEquals(ss, stockService.addStock(ss));
+}
+
+
+@Test
+public void deleteStockTest() {
+	Stock ss = new Stock("libelleStock2", 20, 5);
+	stockService.deleteStock((long) 1);
+	verify(stockRepository).deleteById((long) 1);
+
+}
+
+@Test
+public void updatetStockTest() {
+	Stock ss = new Stock("libelleStock2", 20, 5) ;
+	Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(ss);
+	ss.setLibelleStock("mohamed");
+	Stock exisitingOp= stockService.updateStock(ss) ;
+	
+	assertNotNull(exisitingOp);
+	assertEquals("mohamed", ss.getLibelleStock());
+}
+
+
+
+
+
+
+}

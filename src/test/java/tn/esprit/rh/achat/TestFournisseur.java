0d2c8c0304d2ec0d249c7e18274a119983975fb1
nourmanai <48212954+nourmanai@users.noreply.github.com>
2022-11-13 18:33:48 +0100

    Update TestFournisseur.java

--- src/test/java/tn/esprit/rh/achat/TestFournisseur.java
+++ src/test/java/tn/esprit/rh/achat/TestFournisseur.java
@@ -2,37 +2,34 @@ package tn.esprit.rh.achat;
 
 
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import tn.esprit.rh.achat.entities.CategorieFournisseur;
 import tn.esprit.rh.achat.entities.DetailFournisseur;
-import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.entities.Fournisseur;
-import tn.esprit.rh.achat.entities.SecteurActivite;
 import tn.esprit.rh.achat.repositories.DetailFournisseurRepository;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
-import tn.esprit.rh.achat.repositories.ProduitRepository;
 import tn.esprit.rh.achat.repositories.SecteurActiviteRepository;
 import tn.esprit.rh.achat.services.FournisseurServiceImpl;
 import tn.esprit.rh.achat.services.SecteurActiviteServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 
-class TestFournisseur {
+public class TestFournisseur {
 	
 	
 	
@@ -65,7 +62,6 @@ class TestFournisseur {
 		df.setEmail("email");
 		df.setMatricule("matricule");
 		df.setDateDebutCollaboration(null);
-		//df.setFournisseur(f);
 		detailFournisseurRepository.save(df);
 	
 		f.setCode("code");
@@ -84,7 +80,7 @@ class TestFournisseur {
 	void TestRetrieveAll() {
 		
 		
-			when(fournisseurRepository.findAll()).thenReturn(Stream.of(
+		when(fournisseurRepository.findAll()).thenReturn(Stream.of(
                 new Fournisseur( "code",  "libelle1", null, null,
             			null, null),
                 new Fournisseur("code2",  "libelle2", null, null,
@@ -93,6 +89,7 @@ class TestFournisseur {
             			null, null))
                 .collect(Collectors.toList()));
 		assertEquals(3,fournisseurService.retrieveAllFournisseurs().size());
+		
 		
 	}
 	
@@ -117,40 +114,27 @@ class TestFournisseur {
 			verify(fournisseurRepository).deleteById(f.getIdFournisseur());	
 		 
 	 }
-	/* @Test
+/*@Test
 	 void assignSecteurActiviteToFournisseurTest() {
-		    Fournisseur f=new Fournisseur();
 			DetailFournisseur df= new DetailFournisseur();
-			SecteurActivite sa = new SecteurActivite();*/
-
-		   /* df.setAdresse("adresse");
-			df.setEmail("email");
-			df.setMatricule("matricule");
-			df.setDateDebutCollaboration(null);
-			//df.setFournisseur(f);
-			detailFournisseurRepository.save(df);*/
-			
-		/*	sa.setCodeSecteurActivite("code");
+		    Fournisseur f=new Fournisseur( "code",  "libelle1", null, null,
+        			null,df);
+		
+			SecteurActivite sa = new SecteurActivite();
+		
+			sa.setCodeSecteurActivite("code");
 			sa.setCodeSecteurActivite("libelle");
 			sa.setIdSecteurActivite((long) 1);
 			secteurActiviteRepository.save(sa);
-			//SecteurActiviteService.addSecteurActivite(sa);
-			
-		    f.setCode("code");
-			f.setLibelle("libelle");
-			f.setCategorieFournisseur(CategorieFournisseur.ORDINAIRE);
-
-			
-			
-			
+					
 			fournisseurService.assignSecteurActiviteToFournisseur(sa.getIdSecteurActivite(), f.getIdFournisseur());
 			verify(fournisseurService).assignSecteurActiviteToFournisseur(sa.getIdSecteurActivite(), f.getIdFournisseur());		     
 			
-	 }
-	 @Test
+	 }*/
+	/*@Test
 	 void UpdateFournisseurTest() {
 		 
-		     DetailFournisseur df= new DetailFournisseur( "email", null , "adresse", " matricule", null);
+	    	 DetailFournisseur df= new DetailFournisseur( "email", null , "adresse", " matricule", null);
 	    	 detailFournisseurRepository.save(df);
 		    Fournisseur f=new Fournisseur("code",  "libelle1", null, null,null, df);
 			
@@ -162,8 +146,6 @@ class TestFournisseur {
 			
 			assertNotNull(exisitingOp);
 			assertEquals("libelle2", f.getLibelle());
-		 
-		 
 	 }*/
 	   
 }
